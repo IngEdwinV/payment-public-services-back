832c60050e360e3d109ea09bae4d6265fe93bceb
Edwin Fernando Vasquez Molina <86685519+IngEdwinV@users.noreply.github.com>
2024-03-01 14:55:09 -0500

    Update FacturaService.java
    se agrea función para crear factura

--- src/main/java/edu/escuelaing/arem/ASE/app/service/FacturaService.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/service/FacturaService.java
@@ -1,10 +1,13 @@
 package edu.escuelaing.arem.ASE.app.service;
 
+import edu.escuelaing.arem.ASE.app.configuration.InternalConstantValue;
 import edu.escuelaing.arem.ASE.app.excepcion.Exception;
 import edu.escuelaing.arem.ASE.app.model.Factura;
 import edu.escuelaing.arem.ASE.app.model.usuario;
 import org.springframework.stereotype.Service;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Objects;
 
 @Service
 public class FacturaService {
@@ -27,18 +30,22 @@ public class FacturaService {
             throw new Exception.UsuarioNotFoundException("No se encontró el usuario con ID: " + factura.getIdUsuario());
         }
 
+        // Verificar si existe el servicio
+        if (!servicioExiste(factura.getIdServicio())) {
+            throw new Exception.ServicioNotFoundException("No se encontró el servicio con ID: " + factura.getIdServicio());
+        }
+
         // Puedes agregar más validaciones según tus necesidades
 
-        String sql = "INSERT INTO factura (id, id_usuario, id_servicio, valor, estado, fecha_factura, id_trx) " +
+        String sql = "INSERT INTO factura (id, id_usuario, id_servicio, valor, estado, fecha_factura) " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
         jdbcTemplate.update(sql, new Object[]{
                 factura.getIdUsuario(),
                 factura.getIdServicio(),
                 factura.getValor(),
-                factura.getEstado(),
+                InternalConstantValue.EstadoFactura.ACTIVO,
                 factura.getFechaFactura(),
-                factura.getIdTrx()
         });
     }
 
@@ -54,6 +61,11 @@ public class FacturaService {
     private Boolean usuarioExiste(int userId) {
         usuario us = user.getUsuario(userId);
         return us.getId() == userId;
+    }
+
+    private Boolean servicioExiste(int serviceId) {
+        String serv =service.getService(serviceId);
+        return Objects.equals(serv, serviceId);
     }
 }
 
